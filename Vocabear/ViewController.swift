bfec64f4fef8e8f02800949ed3f4b394854ee5b0
Lev <lmartens_43@hotmail.com>
2021-01-24 15:40:01 +1100

    Initial Commit

---
+++ Vocabear/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vocabear
+//
+//  Created by Lev Martens on 24/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
